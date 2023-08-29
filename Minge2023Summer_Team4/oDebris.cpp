70d08d6517729b2ce418c6830d299fb12ff08f01
hakusa-aizu <soujidecemb@gmail.com>
2023-08-29 14:45:52 +0900

    各種アップデート、テストデブリ君追加

--- Minge2023Summer_Team4/oDebris.cpp
+++ Minge2023Summer_Team4/oDebris.cpp
@@ -3,7 +3,8 @@
 
 Debris::Debris()
 {
-	
+	//for test
+	pos = Vec2{ 200,200 };
 }
 
 Debris::~Debris()
--- Minge2023Summer_Team4/pObjectManager.h
+++ Minge2023Summer_Team4/pObjectManager.h
@@ -2,11 +2,13 @@
 #include "Define.h"
 #include "Figure.h"
 #include "oPlayer.h"
+#include "oDebris.h"
 
 class ObjectManager
 {
 private:
 	Player player;
+	Debris testdebris;
 
 public:
 	ObjectManager();
