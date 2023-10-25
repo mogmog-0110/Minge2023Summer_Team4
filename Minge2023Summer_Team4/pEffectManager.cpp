8d215b573db5a02f1453f8afcaed1d8fed3bdf5b
hakusa-aizu <soujidecemb@gmail.com>
2023-10-25 16:30:34 +0900

    save

---
+++ Minge2023Summer_Team4/pEffectManager.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include "pEffectManager.h"
+
+EffectManager* EffectManager::instance = nullptr;
+
+EffectManager::EffectManager()
+{
+}
+
+EffectManager::~EffectManager()
+{
+}
+
+EffectManager* EffectManager::getInstance()
+{
+	return instance;
+}
+
+void EffectManager::create()
+{
+	if (instance == nullptr)
+	{
+		instance = new EffectManager();
+	}
+}
+
+void EffectManager::destroy()
+{
+	delete instance;
+	instance = nullptr;
+}
---
+++ Minge2023Summer_Team4/pEffectManager.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "src/Define.h"
+
+
+class EffectManager
+{
+private:
+	EffectManager();
+	~EffectManager();
+
+	EffectManager(const EffectManager&) = delete;
+	EffectManager& operator=(const EffectManager&) = delete;
+
+	// 唯一のインスタンスを格納する静的メンバー変数
+	static EffectManager* instance;
+
+public:
+
+	// 唯一のインスタンスを取得するための静的メソッド
+	static EffectManager* getInstance();
+
+	// 唯一のインスタンスを作成
+	static void create();
+
+	// デストラクタ
+	static void destroy();
+
+
+	void update();
+};
